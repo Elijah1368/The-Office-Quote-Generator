cdf04fb2182bc23edd794768f51a3178127fc075
Elijah Amian <eramian125@gmail.com>
2020-01-28 14:57:42 -1000

    Set src folders for partition

--- random-quote-generator/src/index.js
+++ random-quote-generator/src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
+
+import React from '../node_modules/react';
+import ReactDOM from '../node_modules/react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+$(document).ready(function(){
+    if (window.top == window.self) console.log("it is the top most");
+    /*
+    getQuotes().then(()=>{
+        getQuote();
+    });*/
+})
