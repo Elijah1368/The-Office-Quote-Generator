f96e37ae75cfcd5d55ebdd0f044331c56a761b6c
Elijah1368 <eramian125@gmail.com>
2020-02-17 09:21:50 -1000

    Update ReadMe

---
+++ build/precache-manifest.19b8ed30d1136fd937145c44066fb1ed.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d56ef104a4bca081af3e0a826752b5",
+    "url": "/The-Office-Quotes/index.html"
+  },
+  {
+    "revision": "fedd0b7f7c43e0bb74e0",
+    "url": "/The-Office-Quotes/static/css/main.169a09d3.chunk.css"
+  },
+  {
+    "revision": "73aa6cd5d9108a627155",
+    "url": "/The-Office-Quotes/static/js/2.2b18dd14.chunk.js"
+  },
+  {
+    "revision": "52dfbeda0d8fcd66821ea0be571eb11e",
+    "url": "/The-Office-Quotes/static/js/2.2b18dd14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fedd0b7f7c43e0bb74e0",
+    "url": "/The-Office-Quotes/static/js/main.a9595072.chunk.js"
+  },
+  {
+    "revision": "a00c8b9f6b1786f66917",
+    "url": "/The-Office-Quotes/static/js/runtime-main.bc1b65cc.js"
+  },
+  {
+    "revision": "b16d7003940c6a32a6ae542529490dcf",
+    "url": "/The-Office-Quotes/static/media/Andy.b16d7003.png"
+  },
+  {
+    "revision": "3eb0b2b62453ff082ae96ddd3f631d15",
+    "url": "/The-Office-Quotes/static/media/Angela.3eb0b2b6.png"
+  },
+  {
+    "revision": "ee51db8949e0a6b05ff3f0a16128b384",
+    "url": "/The-Office-Quotes/static/media/Creed.ee51db89.png"
+  },
+  {
+    "revision": "25c867483daefedd0aa499c6cc88e0ec",
+    "url": "/The-Office-Quotes/static/media/Dwight.25c86748.png"
+  },
+  {
+    "revision": "f25339690c6ed0ab9c65a35411c84061",
+    "url": "/The-Office-Quotes/static/media/Jim.f2533969.png"
+  },
+  {
+    "revision": "f2d9b09936d8e218cd530d0458c6c4f5",
+    "url": "/The-Office-Quotes/static/media/Kelly.f2d9b099.png"
+  },
+  {
+    "revision": "3e71b695d9dcc9e850a987ac881b6f80",
+    "url": "/The-Office-Quotes/static/media/Kevin.3e71b695.png"
+  },
+  {
+    "revision": "36ce83bd6d1851fc09a77c6484cb3b90",
+    "url": "/The-Office-Quotes/static/media/Meredith.36ce83bd.png"
+  },
+  {
+    "revision": "564d0425e985cc38f0e4bdb2033bb11e",
+    "url": "/The-Office-Quotes/static/media/Michael.564d0425.png"
+  },
+  {
+    "revision": "0f253e0b2d2a61c2b6f3d5a0ed6ae63a",
+    "url": "/The-Office-Quotes/static/media/Oscar.0f253e0b.png"
+  },
+  {
+    "revision": "fa8ad7735db6deba7066571c8af6c5ae",
+    "url": "/The-Office-Quotes/static/media/Pam.fa8ad773.png"
+  },
+  {
+    "revision": "038dcc469e0ed4e366d7a04645a36741",
+    "url": "/The-Office-Quotes/static/media/Phyllis.038dcc46.png"
+  },
+  {
+    "revision": "2f17ef77765883beecab82f9e86bc61f",
+    "url": "/The-Office-Quotes/static/media/Ryan.2f17ef77.png"
+  },
+  {
+    "revision": "595788b9c41485a2cce038f9904d6c29",
+    "url": "/The-Office-Quotes/static/media/Stanley.595788b9.png"
+  },
+  {
+    "revision": "7723fcc2f7234812d9a9668130a87135",
+    "url": "/The-Office-Quotes/static/media/Toby.7723fcc2.png"
+  }
+]);
